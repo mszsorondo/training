4ab7f666226453ac7001b31ee7afa3eb901df264
msorondo <msorondo@live.com.ar>
2024-10-16 20:43:57 -0300

    data download and format conversion

--- single_stage_detector/scripts/fiftyone_openimages_hardcoded.py
+++ single_stage_detector/scripts/fiftyone_openimages_hardcoded.py
@@ -3,13 +3,16 @@
 import os
 import json
 #import argparse
+#if error mongodb: 
+#os.environ["FIFTYONE_DATABASE_VALIDATION"] = "0"
 import fiftyone as fo
 import fiftyone.zoo as foz
 
 #parser = argparse.ArgumentParser(description='Download OpenImages using FiftyOne', add_help=True)
 # parser.add_argument('--dataset-dir', default='/open-images-v6', help='dataset download location')
-dataset_dir = '/open-images-v6'
- 
+
+dataset_dir = r'C:/Users/msoro/fiftyone/open-images-v6'
+
 #parser.add_argument('--splits', default=['train', 'validation'], choices=['train', 'validation', 'test'],
  #                   nargs='+', type=str,
   #                  help='Splits to download, possible values are train, validation and test')
@@ -21,18 +24,16 @@ classes = None
 #parser.add_argument('--output-labels', default='labels.json', type=str,
  #                   help='Classes to download. default to all classes')
 
-output_labels = 'labels.json'
+output_labels = 'openimages-mlperf.json'
 #args = parser.parse_args()
 max_samples=300
 
 print("Downloading open-images dataset ...")
 dataset = foz.load_zoo_dataset(
-    name="open-images-v6",
+    name_or_url="open-images-v6",
     classes=classes,
     splits=splits,
-    label_types="detections",
     dataset_name="open-images",
-    dataset_dir=dataset_dir,
     max_samples=max_samples
 )
 
@@ -40,12 +41,14 @@ print("Converting dataset to coco format ...")
 for split in splits:
     output_fname = os.path.join(dataset_dir, split, "labels", output_labels)
     split_view = dataset.match_tags(split)
+    #breakpoint()
+    #split_view.export(labels_path=output_fname, dataset_type=fo.types.COCODetectionDataset,label_field="detections",classes=classes)
     split_view.export(
         labels_path=output_fname,
         dataset_type=fo.types.COCODetectionDataset,
-        label_field="detections",
+        label_field="ground_truth", #si tira error
+        #label_field='detections_detections',
         classes=classes)
-
     # Add iscrowd label to openimages annotations
     with open(output_fname) as fp:
         labels = json.load(fp)
